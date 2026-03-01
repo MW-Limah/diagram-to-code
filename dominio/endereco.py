2778c7e4edc1fbb0ddc314a555bdd53fcda2c101
MW-Limah <cdrmw16@gmail.com>
2026-03-01 14:27:40 +0000

    Endereco e contato (só revisando e alterando docstrings)

--- dominio/endereco.py
+++ dominio/endereco.py
@@ -1,12 +1,10 @@
 class Endereco:
-    """ Endereço que uma pessoa pode ter """
-    # Atributos
-    def __init__(self, cep: str, estado: str, cidade: str, rua: str, numero: str, complemento: str) -> None:
+    """Representa um endereço."""
+
+    def __init__( self, cep: str, estado: str, cidade: str, rua: str, numero: str, complemento: str) -> None:
         self.cep = cep
         self.estado = estado
         self.cidade = cidade
         self.rua = rua
         self.numero = numero
         self.complemento = complemento
-
-    # Sem métodos
