df8cd750c5cda6160fefd380df6da48a29149a28
MW-Limah <cdrmw16@gmail.com>
2026-03-01 15:10:39 +0000

    Base com formatação genérica. Resultados do teste estão vindo com uma formatação estranha.
    CLIENTE: <dominio.cliente.Cliente object at 0x00000205BFA01D30>

--- dominio/base.py
+++ dominio/base.py
@@ -1,5 +1,9 @@
 class Base:
-    """Classe para entidades do domínio."""
-    
+    """Classe base para entidades do domínio."""
+
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__}>"
+        atributos = ", ".join(
+            f"{chave}={valor}"
+            for chave, valor in self.__dict__.items()
+        )
+        return f"{self.__class__.__name__}({atributos})"
