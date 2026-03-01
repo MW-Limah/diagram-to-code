d5b87de17a02192ee303dd0c6ef80627af681176
MW-Limah <cdrmw16@gmail.com>
2026-03-01 14:28:59 +0000

    Pessoa tem relação com Endereço e Contato

--- dominio/pessoa.py
+++ dominio/pessoa.py
@@ -1,13 +1,17 @@
+from typing import List
+from .contato import Contato
+from .endereco import Endereco
 from datetime import date
+
 
 class Pessoa:
     """Representa uma pessoa do sistema."""
 
-    # Atributos
-    def __init__(self, codigo: int, nome: str, data_nascimento: date, identidade: str) -> None:
+    def __init__( self, codigo: int, nome: str, data_nascimento: date, identidade: str) -> None:
         self.codigo = codigo
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.identidade = identidade
 
-    # Sem métodos
+        self.contatos: List[Contato] = []
+        self.endereco: Endereco | None = None
