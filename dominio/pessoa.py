5535dc09cd37b0c40a0a016ff8c9646bc86da5b1
MW-Limah <cdrmw16@gmail.com>
2026-03-01 13:15:00 +0000

    Classe pessoa

---
+++ dominio/pessoa.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+class Pessoa:
+    """Representa uma pessoa do sistema."""
+    def __init__(self, codigo: int, nome: str, data_nascimento: date, identidade: str) -> None:
+        self.codigo = codigo
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.identidade = identidade
