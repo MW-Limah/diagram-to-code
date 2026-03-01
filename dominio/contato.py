a9d7f873a6f0a7860dc423df567effa69b7fcaaf
MW-Limah <cdrmw16@gmail.com>
2026-03-01 14:25:25 +0000

    Fornecedor associado aos contatos e endereco

--- dominio/contato.py
+++ dominio/contato.py
@@ -1,6 +1,6 @@
 class Contato:
     """Representa os contatos que uma pessoa ou fornecedor pode."""
-    # Atributos
+    # Sem métodos
     def __init__(self, tel1: str, tel2: str, cel1: str, cel2: str, email: str) -> None:
         self.tel1 = tel1
         self.tel2 = tel2
--- dominio/fornecedor.py
+++ dominio/fornecedor.py
@@ -1,12 +1,18 @@
+from typing import List
+from .contato import Contato
+from .endereco import Endereco
+
+
 class Fornecedor:
-    
-    # Atributos
+    """Representa um fornecedor."""
+
     def __init__(self, cod_fornecedor: int, razao: str, nome_fantasia: str, insc_estadual: str, cnpj: str) -> None:
         self.cod_fornecedor = cod_fornecedor
         self.razao = razao
         self.nome_fantasia = nome_fantasia
         self.insc_estadual = insc_estadual
         self.cnpj = cnpj
-
-         
-    # Sem métodos
+ 
+        # Relacionamentos
+        self.contatos: List[Contato] = []
+        self.endereco: Endereco | None = None
